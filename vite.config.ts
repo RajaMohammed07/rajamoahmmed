9809010d8b44fcc3a49e33668cf056dec8761220
RajaMohammed07 <rajamohammed.m2021rvs@gmail.com>
2025-12-07 20:03:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // IMPORTANT for GitHub Pages
-  base: "/rajamoahmmed/",
-
+  base: "/rajamoahmmed/",  // ⭐ important for GitHub Pages
   server: {
     host: "::",
     port: 8080,
@@ -24,3 +21,4 @@ export default defineConfig(({ mode }) => ({
     include: ["react", "react-dom"],
   },
 }));
+
